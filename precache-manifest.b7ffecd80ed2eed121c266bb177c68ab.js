30e3431fa3b890e90fb7cdeff4cf95b53f35f4fb
Kuba Janča <kubajanca@NB19080.local>
2019-09-06 19:14:08 +0200

    Updates

---
+++ precache-manifest.b7ffecd80ed2eed121c266bb177c68ab.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720f7a449de07ad59abd37c6162c94f1",
+    "url": "/restaurant-design/index.html"
+  },
+  {
+    "revision": "76b148cf266352f00f2b",
+    "url": "/restaurant-design/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "50ed180974f5dcb43994",
+    "url": "/restaurant-design/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "76b148cf266352f00f2b",
+    "url": "/restaurant-design/static/js/2.6498d9a7.chunk.js"
+  },
+  {
+    "revision": "50ed180974f5dcb43994",
+    "url": "/restaurant-design/static/js/main.5f33faca.chunk.js"
+  },
+  {
+    "revision": "1f997da3a5eb70b7de2a",
+    "url": "/restaurant-design/static/js/runtime~main.c55b6add.js"
+  },
+  {
+    "revision": "6cc8d199c804b23df6641f9db42da76b",
+    "url": "/restaurant-design/static/media/chef.6cc8d199.png"
+  },
+  {
+    "revision": "0a851e403680d0a9f9f02a751a1f6bf1",
+    "url": "/restaurant-design/static/media/mainChef.0a851e40.png"
+  },
+  {
+    "revision": "4244e69389cda4c09fe0fd7763f1b383",
+    "url": "/restaurant-design/static/media/mainImage.4244e693.jpg"
+  },
+  {
+    "revision": "af1fd66316070a44d76046854169cf82",
+    "url": "/restaurant-design/static/media/mainImageMeal.af1fd663.jpg"
+  },
+  {
+    "revision": "98cb64eb89098bcc0b5bea8838594776",
+    "url": "/restaurant-design/static/media/menuImage1.98cb64eb.jpg"
+  },
+  {
+    "revision": "63b79c40abe4a628415010e9a8055f2a",
+    "url": "/restaurant-design/static/media/menuImage10.63b79c40.jpeg"
+  },
+  {
+    "revision": "f57b784c9da8fca7b23b13decd02de92",
+    "url": "/restaurant-design/static/media/menuImage2.f57b784c.jpg"
+  },
+  {
+    "revision": "857226e8819f632675e246159db0004e",
+    "url": "/restaurant-design/static/media/menuImage3.857226e8.jpg"
+  },
+  {
+    "revision": "7adf8e5e47d73c82a41dbacc123dc8e5",
+    "url": "/restaurant-design/static/media/menuImage4.7adf8e5e.jpg"
+  },
+  {
+    "revision": "260102ae48a0ac001150fb6de8f6728c",
+    "url": "/restaurant-design/static/media/menuImage5.260102ae.jpg"
+  },
+  {
+    "revision": "1849ab485436b81f9952296ec1c27c3d",
+    "url": "/restaurant-design/static/media/menuImage6.1849ab48.jpg"
+  },
+  {
+    "revision": "4e76db5799111b71fa6e32aed7f1b52b",
+    "url": "/restaurant-design/static/media/menuImage7.4e76db57.jpg"
+  },
+  {
+    "revision": "eb7db189e6d460457de0ace3f0d54a9b",
+    "url": "/restaurant-design/static/media/menuImage8.eb7db189.jpg"
+  },
+  {
+    "revision": "9b860d97ee2b07043fdf49c4f64230e0",
+    "url": "/restaurant-design/static/media/menuImage9.9b860d97.jpg"
+  }
+]);
